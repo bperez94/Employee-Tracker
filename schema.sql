97084b709e4c14487213d62bb5ec263c1fea8f99
Brandon Perez <brandonp940@gmail.com>
2020-08-06 08:53:12 -0400

    fixed formatting to work in work bench

--- schema.sql
+++ schema.sql
@@ -1,8 +1,30 @@
-/*DROP DATABASE IF EXISTS 
-CREATE database 
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
 
-USE 
+USE employeeDB;
 
-CREATE TABLE (
-
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    department_Name VARCHAR(30)
 );
+
+CREATE TABLE position (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
+
+SELECT * FROM department;
+SELECT * FROM position;
+SELECT * FROM employee;
+
+
