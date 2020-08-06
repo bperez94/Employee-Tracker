60577e741bf4b8c0a2b6f64646716d30989e2e49
Brandon Perez <brandonp940@gmail.com>
2020-08-06 08:51:01 -0400

    sql connection working

--- employee.js
+++ employee.js
@@ -1,22 +1,39 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+//const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Your port
   port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
-  password: "",
-  database: ""
+  password: "lowercase",
+  database: "employeeDB"
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  whichDepart();
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
-});
+connection.end();
+
+/*function whichDepart() {
+  inquirer
+    .prompt ({
+      name: "department",
+      type: "input",
+      message: "what department do you work for?"
+    })
+    .then(function(answer) {
+      console.log(answer);
+    })
+    .catch((err) => {
+      
+    });
+}*/
 
