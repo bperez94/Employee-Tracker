a86453c8fe4582ccdaa2263c1f0fb9bc4404fe6d
Brandon Perez <brandonp940@gmail.com>
2020-08-12 09:16:47 -0400

    addded db folder to imporove file management

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id,department_Name) VALUES (1,"Accounting");
+INSERT INTO department (id,department_Name) VALUES (2,"Warehouse");
+
+INSERT INTO position (id,title,salary,department_id) VALUES (40,"Senoir Accountant", 80000,1);
+INSERt INTO position  (id,title,salary,deoartment_id) VALUES (30,"foreman",65000,2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("john","smith",10,20)
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("jane", "doe",12,24);
